6945f218103ab6c9a99f75d8e21eb79dadd07151
Brendan Beerman <brendanbeerman@gmail.com>
2017-03-29 20:24:32 -0300

    Part 3 and 4

---
+++ src/main/java/dal/gravity/GravityConstant.java
@@ -0,0 +1,16 @@
+package dal.gravity;
+
+public class GravityConstant implements GravityModel
+{
+	public GravityConstant(double g)
+	{
+		
+	}
+	
+	@Override
+	public double getGravitationalField()
+	{
+		return 0;
+	}
+
+}
